d1c6ae81112ca065a6769dd3a19460a7877771fc
raymanaeron <raymana@live.com>
2025-04-16 22:43:20 -0700

    Logger init fixes

--- logger/src/logger_loader.rs
+++ logger/src/logger_loader.rs
@@ -1,10 +1,11 @@
-use std::sync::{Arc, OnceLock};
-use crate::log_contracts::{LogLevel, LogDestination};
+use std::sync::{ Arc, OnceLock };
+use crate::log_contracts::{ LogLevel, LogDestination };
 use crate::log_config::LoggingConfig;
 use crate::file_log_destination::FileLogDestination;
 use crate::http_log_destination::HttpLogDestination;
 use crate::LogWriter;
 use crate::Logger;
+use crate::TelemetryConfig;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
 use toml;
@@ -16,13 +17,19 @@ pub static LOGGER: OnceLock<Arc<dyn Logger>> = OnceLock::new();
 impl LoggerLoader {
     pub fn load(config: &LoggingConfig) -> Arc<dyn Logger> {
         let log_dest: Arc<dyn LogDestination> = match config.r#type.as_str() {
-            "http" => Arc::new(HttpLogDestination::new(
-                config.http_endpoint.as_ref().expect("Missing http_endpoint")
-            )),
-            "file" => Arc::new(FileLogDestination::new(
-                config.file_path.as_ref().expect("Missing file_path").into(),
-                10 * 1024 * 1024,
-            )),
+            "http" =>
+                Arc::new(
+                    HttpLogDestination::new(
+                        config.http_endpoint.as_ref().expect("Missing http_endpoint")
+                    )
+                ),
+            "file" =>
+                Arc::new(
+                    FileLogDestination::new(
+                        config.file_path.as_ref().expect("Missing file_path").into(),
+                        10 * 1024 * 1024
+                    )
+                ),
             _ => panic!("Unknown logger type"),
         };
 
@@ -40,21 +47,19 @@ impl LoggerLoader {
 
     pub async fn init(config_path: &str) {
         // Read configuration from the provided path
-        let mut config_file = File::open(config_path)
-            .await
-            .expect("Unable to open configuration file");
+        let mut config_file = File::open(config_path).await.expect(
+            "Unable to open configuration file"
+        );
         let mut contents = String::new();
-        config_file
-            .read_to_string(&mut contents)
-            .await
-            .expect("Failed to read configuration file");
+        config_file.read_to_string(&mut contents).await.expect("Failed to read configuration file");
 
-        // Parse the TOML configuration into LoggingConfig
-        let logging_config: LoggingConfig =
-            toml::from_str(&contents).expect("Error parsing configuration file");
+        // Parse the TOML configuration into TelemetryConfig
+        let telemetry_config: TelemetryConfig = toml
+            ::from_str(&contents)
+            .expect("Error parsing configuration file");
 
         // Load the logger using the logging configuration
-        let logger = LoggerLoader::load(&logging_config);
+        let logger = LoggerLoader::load(&telemetry_config.logging);
 
         // Store the logger in the global LOGGER variable
         LOGGER.set(logger).expect("Logger already initialized");
